49415450c54743c9f11a01646be0f584a9ea0d15
thisishwan2 <feel000617@gmail.com>
2024-04-22 20:30:18 +0900

    feat: 저장된 시나리오 불러오기, 계층 정보 추출 및 스크린샷 저장

--- server/service.py
+++ server/service.py
@@ -1,6 +1,9 @@
+import os
 import re
 import subprocess
 import time
+from pathlib import Path
+from pymongo import ASCENDING
 
 from com.dtmilano.android.viewclient import ViewClient
 from flask import Flask, request, jsonify
@@ -13,6 +16,7 @@ from server import app
 
 #Parse XML
 tree = elemTree.parse('keys.xml')
+
 openai.api_key = tree.find('string[@name="OPEN_API_KEY"]').text
 AWS_ACCESS_KEY = tree.find('string[@name="AWS_ACCESS_KEY"]').text
 AWS_SECRET_KEY = tree.find('string[@name="AWS_SECRET_KEY"]').text
@@ -21,8 +25,6 @@ location = 'ap-northeast-2'
 
 # 시리얼 번호
 serial_no = None
-
-
 
 # S3 연결
 def s3_connection():
@@ -46,7 +48,7 @@ def s3_connection():
 s3 = s3_connection()
 
 # s3에 파일 업로드
-def s3_put_object(s3, BUCKET_NAME, AWS_ACCESS_KEY, file):
+def s3_put_object(file_dir):
     '''
     s3 bucket에 지정 파일 업로드
     :param s3: 연결된 s3 객체(boto3 client)
@@ -55,37 +57,47 @@ def s3_put_object(s3, BUCKET_NAME, AWS_ACCESS_KEY, file):
     :return: 성공 시 True, 실패 시 False 반환
     '''
     try:
+        upload_name = file_dir.split('/')[-1]
         s3.upload_file(
-            BODY = file,
+            Filename = file_dir,
             Bucket = BUCKET_NAME,
-            Key = AWS_ACCESS_KEY)
+            Key = upload_name
+        )
 
-        image_url = f'https://{BUCKET_NAME}.s3.{location}.amazonaws.com/{file}'
+        image_url = f'https://{BUCKET_NAME}.s3.{location}.amazonaws.com/{upload_name}'
     except Exception as e:
         print(e)
         return False
     return image_url
 
-# 디바이스의 현재 화면 스크린샷 (수정)
+# 디바이스의 현재 화면 스크린샷
 def take_screenshot():
     # 현재 시간을 이용하여 파일 이름 생성
-    current_time = time.strftime("%H_%M_%S", time.localtime())
+    current_time = "screenshot_"+ time.strftime("%H_%M_%S", time.localtime())
+
+    # 사용자의 데스크톱 경로 가져오기
+    screenshot_path = Path.home() / 'adb_screenshot'
+    screenshot_path.mkdir(parents=True, exist_ok=True)  # 폴더가 없으면 생성
+
+    # 스크린샷 저장할 파일의 전체 경로
+    screenshot_file = screenshot_path / f"{current_time}.png"
 
     # adb 명령을 실행하여 스크린샷 캡처
     capture_command = "adb shell screencap -p /sdcard/screen.png"
     subprocess.run(capture_command, shell=True)
 
     # 스크린샷 파일을 로컬로 가져오기
-    pull_command = f"adb pull /sdcard/screen.png ~/Desktop/{current_time}.png"
+    pull_command = f"adb pull /sdcard/screen.png {screenshot_file}"
     subprocess.run(pull_command, shell=True)
 
     # 디바이스에서 스크린샷 파일 삭제
     delete_command = "adb shell rm /sdcard/screen.png"
     subprocess.run(delete_command, shell=True)
 
-    print(f"스크린샷이 ~/Desktop/{current_time}.png에 저장되었습니다.")
+    print(f"스크린샷이 {screenshot_file}에 저장되었습니다.")
+    return str(screenshot_file) # 로컬 경로 반환
 
-# 디바이스 연결 확인(+s3 연결)
+# 디바이스 연결 확인
 def adb_connect():
     global serial_no
 
@@ -114,22 +126,32 @@ def start_adb_server():
     except subprocess.CalledProcessError as e:
         print("Error:", e)
 
+# 현재 계층 정보 추출 및 DB에 저장
 def current_view():
     global serial_no
     hierarchy = app.config['HIERARCHY']
 
     if request.method == 'POST':
+
+        scenario_num = request.json['scenario_num']
+        task_num = request.json['task_num']
+
         vc = ViewClient(*ViewClient.connectToDeviceOrExit(serialno=serial_no))
         # traverse_to_list 메서드를 사용하여 디바이스의 UI 계층 구조를 리스트로 반환(ViewClient로 부터 재 정의함)
         ui_list = vc.traverse_to_list(transform=vc.traverseShowClassIdTextAndUniqueId)  # vc의 디바이스 UI 트리를 순회하여 리스트로 반환
 
         # 스크린샷을 찍어서 s3에 저장
-        take_screenshot() # 수정
-        s3_put_object()
-        print(ui_list)
+        screenshot_dir = take_screenshot()
+        screenshot_url = s3_put_object(screenshot_dir)
+
+        # 로컬에서 이미지 삭제
+        os.remove(screenshot_dir)
+
+        insert_data = {"scenario_num": scenario_num,
+                       "task_num":task_num,
+                       "screenshot_url": screenshot_url}
 
         # mongodb에 저장
-        ui_data = {}
         for ui in ui_list:
             pattern_line_separator = '\n'
             ui = re.sub(pattern_line_separator, " ", ui)
@@ -141,68 +163,131 @@ def current_view():
             component = match.group(1).strip()  # 앞뒤 공백 제거
             unique_id = match.group(2)
 
-            ui_data[unique_id] = component
+            insert_data[unique_id] = component
 
         # MongoDB 컬렉션에 딕셔너리 리스트를 저장합니다.
-        inserted_data = hierarchy.insert_one(ui_data)
+        inserted_data = hierarchy.insert_one(insert_data)
         object_id = str(inserted_data.inserted_id)
 
-        return jsonify({'message': 'success'}) # 응답으로 이미지 url과 db object id를 반환
+        # 응답으로 이미지 url과 db object id를 반환
+        return jsonify({
+            'object_id': object_id,
+            'screenshot_url': screenshot_url,
+            'scenario_num': scenario_num,
+            'task_num': task_num
+        })
 
 # 액션 저장
 def save_action():
-    hierarchy = app.config['HIERARCHY']
+    action_collection = app.config['ACTION_COLLECTION']
 
     if request.method == 'POST':
+        scenario_num = request.json['scenario_num']
+        action_num = request.json['action_num']
         action = request.json['action']
-        action_document = {'action': action}
-        inserted_data = hierarchy.insert_one(action_document)
+
+        action_document = {'scenario_num': scenario_num,
+                           'action_num': action_num,
+                           'action': action
+                           }
+
+        inserted_data = action_collection.insert_one(action_document)
         object_id = str(inserted_data.inserted_id)
-        return jsonify({'object_id': object_id})
+        return jsonify({'object_id': object_id,
+                        'scenario_num': scenario_num,
+                        'task_num': action_num
+        })
+
+# 액션 수정
+
 
 # 테스트 시나리오 실행
-# @app.route('/run-scenario', methods=['POST'])
-# def run_scenario(): # input, output 의논해보기
+def run_scenario(): # input, output 의논해보기
+    if request.method == 'POST':
+        before_hierachy = request.json['before_hierachy']
+        action = request.json['action']
+        after_hierachy = request.json['after_hierachy']
+
+        hierarchy = app.config['HIERARCHY']
+
+        # 시나리오 실행(before_hierachy 와 action을 GPT에게 입력 후 결과를 받아온다.
+        view_id, run_func = infer_viewid(before_hierachy, action)
+        print(view_id)
+        # 추론한 viewId를 바탕으로 adb 명령을 수행
 
 
-# # DB에 저장되어 있는 시나리오 불러오기
+def infer_viewid(hierarchy, action):
+    '''
+    action을 입력받아서 view id를 추론하는 함수
+    :param hierarchy: 계층 정보
+    :param action: 수행하고자 하는 action
+    :return: 추론된 view id, 실행할 함수
+    '''
+    # action을 GPT에 입력
+    openai.api_key
+    pre_prompt = "한국어로 친절하게 대답해줘. 그리고 view ID만 출력해줘\n\n"
+    response = openai.ChatCompletion.create(
+        model="gpt-4-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful code assistant."},
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": pre_prompt +
+                                             "ui 계층은 다음과 같아" + hierarchy +
+                                             "다음 액션을 수행해줘" + action
+                    }
+                ]
+            }
+        ],
+        max_tokens=3000,
+        temperature=0.5
+    )
+    print(response)
+    answer = response.choices[0].message.content.strip()
+
+    # 추론된 view id를 반환
+    return answer
+
+
+
+# DB에 저장되어 있는 전체 시나리오 및 테스트 불러오기
 def load_scenario():
     hierarchy = app.config['HIERARCHY']
+    action_collection = app.config['ACTION_COLLECTION']
 
     if request.method == 'GET':
-        documents = []
-        for doc in hierarchy.find():
-            doc['_id'] = str(doc['_id'])
-            documents.append(doc)
+        # 시나리오 번호와 작업 번호별로 문서를 오름차순으로 정렬
+        hierarchy_docs = hierarchy.find().sort([
+            ("scenario_num", ASCENDING),  # 먼저 시나리오 번호로 정렬
+            ("task_num", ASCENDING)  # 그 다음 작업 번호로 정렬
+        ])
 
-        return jsonify(documents)
+        action_docs = action_collection.find().sort([
+            ("scenario_num", ASCENDING),  # 먼저 시나리오 번호로 정렬
+            ("action_num", ASCENDING)  # 그 다음 액션 번호로 정렬
+        ])
 
+        hierarchy_dict = {} # 시나리오 번호: 계층 정보(작업 번호, 스크린샷 url, object id)
+        action_dict = {} # 시나리오 번호: 액션 정보(액션, object id)
+        for hierarchy_doc in hierarchy_docs:
 
-# 클라이언트의 input을 전달받음
-def test_gpt(self):
-    if request.method == 'POST':
-        prompt = request.json['prompt']
-        image_url = "https://lily-parakeet-0ba.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F16a65ace-b6d6-4de9-9a32-f07129b8e3a6%2Fec407283-025f-4c9a-9325-b8de86793499%2FUntitled.png?table=block&id=4a6308ee-bd95-4829-b547-3656ca88f0b4&spaceId=16a65ace-b6d6-4de9-9a32-f07129b8e3a6&width=1600&userId=&cache=v2"
-        openai.api_key
-        pre_prompt = "한국어로 친절하게 대답해줘. 그리고 view ID만 출력해줘\n\n"
-        response = openai.chat.completions.create(
-            model="gpt-4-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful code assistant."},
-                {
-                    "role": "user",
-                    "content": [
-                        {"type" : "text", "text": pre_prompt + prompt},
-                        {"type": "image_url", "image_url": {
-                            "url": image_url}
-                        }
-                    ]
-                }
-            ],
-            max_tokens=3000,
-            temperature=0.5
-        )
-        print(response)
-        answer = response.choices[0].message.content.strip()
+            scenario_num = hierarchy_doc['scenario_num']
+            if scenario_num not in hierarchy_dict:
+                hierarchy_dict[scenario_num] = []
+            hierarchy_dict[scenario_num].append({
+                'task_num': hierarchy_doc['task_num'],
+                'screenshot_url': hierarchy_doc['screenshot_url'],
+                'object_id': str(hierarchy_doc['_id'])
+            })
 
-        return jsonify({'answer': answer})
+        for action_doc in action_docs:
+            action_num = action_doc['scenario_num']
+            if action_num not in action_dict:
+                action_dict[action_num] = []
+            action_dict[action_num].append({
+                'action': action_doc['action'],
+                'object_id': str(action_doc['_id'])
+            })
+
+        return jsonify({'hierarchy': hierarchy_dict, 'action': action_dict})
