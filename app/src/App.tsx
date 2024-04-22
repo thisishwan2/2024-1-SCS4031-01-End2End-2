b305fec43117cde1d8b2c44abc30cdd5d4683782
jjm2317 <wlaks2317@gmail.com>
2024-04-23 00:35:11 +0900

    feat: initialize ui

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import {  Grid } from "@mui/material";
+
+
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -13,40 +14,10 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
+    <Grid container columns={16}>
+      <Grid item xs={4}>a</Grid>
+      <Grid item xs={8}>b</Grid>
+    </Grid>
   );
 }
 
