0ff0bcb9325f6ba5322a3ac2f6a1744b948d13b5
thisishwan2 <feel000617@gmail.com>
2024-04-28 00:59:44 +0900

    feat: adb function 정의

---
+++ server/adb_function.py
@@ -0,0 +1,43 @@
+import subprocess
+
+from service import vc
+
+def find_by_id_touch(key):
+    print("id_touch")
+    vc.findViewById('id/no_id/'+key).touch()
+
+def find_by_text_touch(text):
+    print("text_touch")
+    vc.findViewWithText(text).touch()
+
+def back():
+    print("back")
+    cmd = "adb shell input keyevent 4"
+    subprocess.run(cmd, shell=True)
+
+def home():
+    print("home")
+    cmd = "adb shell input keyevent 3"
+    subprocess.run(cmd, shell=True)
+
+def swipe_left_to_right():
+    print("swipe")
+    cmd = "adb shell input swipe 1000 500 100 500 100"
+    subprocess.run(cmd, shell=True)
+
+def swipe_right_to_left():
+    print("swipe")
+    cmd = "adb shell input swipe 100 500 1000 500 100"
+    subprocess.run(cmd, shell=True)
+
+def swipe_up_to_down():
+    print("swipe down")
+    cmd = "adb shell input swipe 500 1000 500 500 100"
+    subprocess.run(cmd, shell=True)
+
+def swife_down_to_up():
+    print("swipe up")
+    cmd = "adb shell input swipe 500 500 500 1000 100"
+    subprocess.run(cmd, shell=True)
+
+
