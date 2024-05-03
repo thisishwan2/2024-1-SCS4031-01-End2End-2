d11ba11b4af62c7529f4b64a276a58ea3864f1c4
thisishwan2 <feel000617@gmail.com>
2024-05-03 21:40:52 +0900

    feat: 시나리오 작업 추가

--- server/routes.py
+++ server/routes.py
@@ -6,24 +6,41 @@ from server import app
 api = Api(app, version='1.0', title='e2e API 문서', description='Swagger 문서', doc="/api-docs")
 e2e = api.namespace(name = "e2e", description='e2e API')
 
-# 개별 시나리오 아이템 모델
+# 개별 시나리오 아이템 응답 모델
 scenario_model = api.model('scenario', {
     'object_id': fields.String(description='Object ID', required=True, attribute=lambda x: str(x['_id'])),
     'scenario_name': fields.String(description='시나리오 이름', required=True),
     'run_status': fields.String(description='시나리오 실행 상태', required=True),
 })
 
-# 시나리오 리스트를 위한 모델
+# 시나리오 리스트 응답 모델
 scenario_list_model = api.model('scenario_list', {
     'scenarios': fields.List(fields.Nested(scenario_model), description='시나리오 리스트')
 })
 
-# 시나리오 상세 정보 모델
+# 시나리오 상세 정보 응답 모델
 scenario_detail_model = api.model('scenario_detail', {
     'object_id': fields.String(description='Object ID', required=True),
     'scenario_name': fields.String(description='시나리오 이름', required=True),
     'run_status': fields.String(description='시나리오 실행 상태', required=True),
 })
+
+# 시나리오 생성 요청 모델
+create_scenario_model = e2e.model('CreateScenario', {
+    'scenario_name': fields.String(description='시나리오 이름', required=True),
+})
+
+# 시나리오 생성 응답 모델
+# create_scenario_model = api.model('CreateScenario', {
+#     'object_id': fields.String(description='Object ID', required=True),
+#     'scenario_name': fields.String(description='시나리오 이름', required=True),
+# })
+
+# 시나리오 작업 추가 요청 모델
+add_task_model = e2e.model('AddTask', {
+    'object_id': fields.String(description='Object ID', required=True),
+})
+
 
 
 current_view = e2e.model('current_view', {
@@ -63,16 +80,6 @@ HierarchyDetail = api.model('HierarchyDetail', {
     'task_num': fields.String(description='작업 번호', required=True)
 })
 
-# 시나리오 전체를 로드하는 모델
-LoadScenario = api.model('LoadScenario', {
-    'action': fields.Nested(api.model('Action', {
-        'actions': fields.List(fields.Nested(ActionDetail))
-    }), description='액션 디테일의 맵', attribute='action'),
-    'hierarchy': fields.Nested(api.model('Hierarchy', {
-        'hierarchies': fields.List(fields.Nested(HierarchyDetail))
-    }), description='계층 구조의 맵', attribute='hierarchy')
-})
-
 run_scenario = e2e.model('scenario', {
     'before_hierachy_id': fields.String(description = 'action 실행 전 계층 objectId', required = True, example = '1'),
     'action': fields.String(description = '수행하고자 하는 action', required = True, example = '1번 id를 찾아서 클릭해줘'),
@@ -93,34 +100,6 @@ class adb_connect(Resource):
         :return: 디바이스 연결 상태
         '''
         return service.adb_connect()
-
-# 현재 계층 정보 추출 및 DB에 저장
-@e2e.route('/current-view')
-@api.doc(responses={200: 'Success', 400: 'Error'},
-            description='이 API 엔드포인트는 현재 계층 정보를 추출하고 데이터베이스에 저장합니다.')
-class current_view(Resource):
-    @e2e.expect(current_view)
-    @api.response(200, 'Success', current_view_response_model)  # 응답 모델 적용
-    def post(self):
-        '''
-        현재 계층 정보 추출 및 DB에 저장
-        :return: 현재 계층 정보
-        '''
-        return service.current_view()
-
-# 액션 저장
-@e2e.route('/save-action')
-@api.doc(responses={200: 'Success', 400: 'Error'},
-         description='이 API 엔드포인트는 사용자의 액션을 받아서 데이터베이스에 저장합니다.')
-class save_action(Resource):
-    @e2e.expect(test_action)
-    @api.response(200, 'Success', action_response_model)  # 응답 모델 적용
-    def post(self):
-        '''
-        액션 저장
-        :return: 액션 아이디
-        '''
-        return service.save_action()
 
 # 시나리오 리스트 불러오기
 @e2e.route('/scenarios')
@@ -146,17 +125,60 @@ class scenario(Resource):
         '''
         return service.scenario(scenario_id)
 
-# DB에 저장되어 있는 시나리오 불러오기
-@e2e.route('/load-scenario')
-class load_scenario(Resource):
-    # @e2e.expect(run_scenario)
-    @api.response(200, 'Success', LoadScenario)  # 응답 모델 적용
-    def get(self):
+# 시나리오 생성
+@e2e.route('/scenarios')
+class create_scenario(Resource):
+    # @e2e.expect(create_scenario_model)
+    @e2e.expect(create_scenario_model)
+    @api.response(200, 'Success')  # 응답 모델 적용
+    def post(self):
         '''
-        DB에 저장되어 있는 시나리오 불러오기
-        :return: 시나리오
+        시나리오 생성
+        :return: 생성된 시나리오 아이디
         '''
-        return service.load_scenario()
+        return service.create_scenario()
+
+# 시나리오 작업 추가
+@e2e.route('/scenarios/tasks')
+class add_task(Resource):
+    @e2e.expect(add_task_model)
+    @api.response(200, 'Success')  # 응답 모델 적용
+    def post(self):
+        '''
+        시나리오 작업 추가
+        :return: 생성된 시나리오 아이디
+        '''
+        return service.add_task()
+
+
+
+# 현재 계층 정보 추출 및 DB에 저장
+@e2e.route('/scenario/<string:scenario_id>/hierarchy')
+@api.doc(responses={200: 'Success', 400: 'Error'},
+            description='이 API 엔드포인트는 현재 계층 정보를 추출하고 데이터베이스에 저장합니다.')
+class current_view(Resource):
+    @e2e.expect(current_view)
+    @api.response(200, 'Success', current_view_response_model)  # 응답 모델 적용
+    def post(self):
+        '''
+        현재 계층 정보 추출 및 DB에 저장
+        :return: 현재 계층 정보
+        '''
+        return service.current_view()
+
+# 액션 저장
+@e2e.route('/save-action')
+@api.doc(responses={200: 'Success', 400: 'Error'},
+         description='이 API 엔드포인트는 사용자의 액션을 받아서 데이터베이스에 저장합니다.')
+class save_action(Resource):
+    @e2e.expect(test_action)
+    @api.response(200, 'Success', action_response_model)  # 응답 모델 적용
+    def post(self):
+        '''
+        액션 저장
+        :return: 액션 아이디
+        '''
+        return service.save_action()
 
 # 시나리오 실행(계층정보 - 액션 - 계층정보)
 @e2e.route('/run-scenario')
